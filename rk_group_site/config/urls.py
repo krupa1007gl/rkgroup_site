d84b07b06f157cda79b521ca127175f77d7819e7
krupa1007gl <krupa.xleb@gmail.com>
2026-03-05 01:19:51 +0300

    rk_group_site

--- rk_group_site/config/urls.py
+++ rk_group_site/config/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path(
+        'yandex_399b62af104e347c.html', 
+        TemplateView.as_view(template_name='yandex_399b62af104e347c.html')
+    ),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('pages.urls')),
