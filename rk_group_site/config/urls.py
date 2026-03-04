f39c3b88ce37a3636dfdcbf60db174a0f6806f89
krupa1007gl <krupa.xleb@gmail.com>
2026-03-05 01:05:39 +0300

    rk_group

--- rk_group_site/config/urls.py
+++ rk_group_site/config/urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse 
 
-def yandex_verify(request):
-    return HttpResponse("Verification: 399b62af104e347c")
 
 urlpatterns = [
-    path('yandex_399b62af104e347c.html', yandex_verify),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('pages.urls')),
